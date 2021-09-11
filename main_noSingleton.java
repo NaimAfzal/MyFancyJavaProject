972fe7e9351d44524b1e1cdefb4e6834357fd018
NaimAfzal <mafzal161010@bscse.uiu.ac.bd>
2021-09-11 21:35:19 +0600

    Create main_noSingleton.java
    Without singleton pattern

---
+++ main_noSingleton.java
@@ -0,0 +1,7 @@
+public class Main {
+   String connection = "You are now connected to the database";
+  public static void main(String[] args) {
+       Main db1 = new Main();
+       System.out.println(db1.connection);
+  }
+}
