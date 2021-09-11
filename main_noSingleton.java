c60719c320bc7e55976571f6c734ee74f1a3ecc6
NaimAfzal <mafzal161010@bscse.uiu.ac.bd>
2021-09-11 21:40:21 +0600

    Singleton pattern

--- main_noSingleton.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-   String connection = "You are now connected to the database";
-  public static void main(String[] args) {
-       Main db1 = new Main();
-       System.out.println(db1.connection);
-  }
-}
